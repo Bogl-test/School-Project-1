455688a38879406f7fbda5f2595dde8096e995a5
Oliver <oliv652e@gmail.com>
2023-11-29 08:21:46 +0100

    Co-authored-by: DoktorBamse <DoktorBamse@users.noreply.github.com>

--- screens.js
+++ screens.js
@@ -1,5 +1,7 @@
 function menuScreen() {
-    frameRate(60)
+    end_screen_start_time = undefined
+    ammo_crate_start_time = undefined
+    arrowY = height - height / 6
     menuX = 0
     while (menuX < width) { // keeps drawing the same gif to fill the menu screen
         image(gif_loadImg, menuX, 0);
@@ -9,40 +11,121 @@ function menuScreen() {
         menuX += 400
     }
     push()
-    stroke(0)
-    strokeWeight(9)
-    textSize(30)
-    textAlign(CENTER)
-    fill(123, 255, 0)
-    text("PRESS ENTER TO PLAY", width / 2, innerHeight / 2 + height / 6)
-    textSize(90)
-    text("IMORTAL MATRIX", innerWidth / 2, innerHeight / 4)
-    pop()
-    // Buttons (Place Holder    )
-    push();
-    fill(0, 255, 0);
     strokeWeight(0)
-    rectMode(CENTER);
-    rect(width / 2, height / 2, 300, 50)
-    fill(0, 0, 0)
-    textSize(30)
-    text("EASY", width / 2 - 150, height / 2 + 10)
-    pop();
+    textSize(200)
+    textAlign(CENTER, CENTER)
+    rectMode(CENTER)
+    fill(0, 0, 0, 190)
+    rect(width / 2, height - height / 3, 700, 100)
+    fill(0, 255, 0)
+    textStyle(ITALIC);
+    text("TITLE TBD", innerWidth / 2, innerHeight / 5)
+    textSize(70)
+    text("Press ENTER to Play", width / 2, height - height / 3)
 
-    // menu_Music.loop()
-    if (keyIsDown(13) && music == false) {
-        Screen = 2
-        menu_Music.stop()
-        // lvl1_Music.loop()
-        // lvl2_Music.loop()
-        //lvl3_Music.loop()
-        music = true
+
+    textSize(80)
+    if (easy_mode) {
+        rect(width / 2, height - height / 6 + 20, 600, 150)
+        fill(0, 0, 0, 230)
+        text("EASY MODE", width / 2, height - height / 6 + 20)
     }
+    else if (medium_mode) {
+        fill("orange")
+        rect(width / 2, height - height / 6 + 20, 600, 150)
+        fill(0, 0, 0, 230)
+        text("MEDIUM MODE", width / 2, height - height / 6 + 20)
+    }
+    else if (hard_mode) {
+        fill(255, 0, 0)
+        rect(width / 2, height - height / 6 + 20, 600, 150)
+        fill(0, 0, 0, 230)
+        text("HARD MODE", width / 2, height - height / 6 + 20)
+    }
+    pop()
+
+
+    push()
+    rectMode(CENTER)
+    noStroke()
+    angleMode(DEGREES);
+    fill(255, 255, 255, 230)
+    translate(width / 2 + 320, arrowY + 25)
+    rotate(45)
+    rect(0, -60, 130, 10)
+    rotate(-90)
+    rect(0, 60, 130, 10)
+    pop()
+
+    push()
+    rectMode(CENTER)
+    noStroke()
+    angleMode(DEGREES);
+    fill(255, 255, 255)
+    translate(width / 2 - 320, arrowY + 25)
+    rotate(-45)
+    rect(0, -60, 130, 10)
+    rotate(90)
+    rect(0, 60, 130, 10)
+    pop()
+    push()
+    if (width / 2 - 310 > mouseX && mouseX > width / 2 - 415 && mouseY < arrowY + 120 && mouseY > arrowY - 70 ||
+        width / 2 + 310 < mouseX && mouseX < width / 2 + 415 && mouseY < arrowY + 120 && mouseY > arrowY - 70) {
+        cursor(HAND)
+    }
+    pop()
+    if (keyIsDown(13) && controls == true) {
+        Screen = 2
+        if (easy_mode) {
+            enemySpeed *= 1
+        }
+        else if (medium_mode) {
+            enemySpeed *= 1.5
+        }
+        else if (hard_mode) {
+            enemySpeed *= 2
+        }
+    }
+
+    else if (keyIsDown(13) && controls == false) {
+        Screen = 1.5
+        controls = true
+        //deciding speed of enemy
+        if (easy_mode) {
+            enemySpeed *= 1
+        }
+        else if (medium_mode) {
+            enemySpeed *= 1.5
+        }
+        else if (hard_mode) {
+            enemySpeed *= 2
+        }
+    }
+
+}
+
+function controls_screen() {
+    if (typeof timer == "undefined") {
+        timer = millis()
+    }
+    background(123)
+    if (keyIsDown(13) && millis() - timer > 1000) {
+        Screen = 2
+        timer = undefined
+
+    }
+
 }
 
 function play_screen() {
-    noCursor()
     image(School_Project_BG, 0, 0)
+    noCursor()
+    if (typeof ammo_crate_start_time === 'undefined') { // if ammo_crate_start_time doesn't have a value then it runs ammo_crate_start_time.
+        ammo_crate_x = random(0 + ammo_crate_size, width - ammo_crate_size);
+        ammo_crate_y = random(0 + ammo_crate_size, height - ammo_crate_size);
+        ammo_crate_start_time = millis();
+    }
+    Ammo_Crate();
     for (let i = 0; i < enemyCount.length; i++) {
         enemyCount[i].draw();
         enemyCount[i].over_lapping()
@@ -51,52 +134,74 @@ function play_screen() {
         bulletCount[i].bulletDraw();
         bulletCount[i].hit()
     }
-    strokeWeight(0)
     player();
+    noStroke
     push();
-    stroke(0, 255, 0)
-    push();
-    noFill()
-    strokeWeight(3)
     textSize(20)
-    fill(255)
-    text("Enemies Killed : " + enemies_Killed, 20, 0 + 40)
-    text("Health : " + playerHealth, 20, 0 + 90)
-    text("XP to level up: " + (xp_to_level_up - xp_count), 20, 0 + 140)
+    fill(49, 113, 63, 230)
+    text("Enemies Killed : " + enemies_Killed, 20, 40)
+    text("Health : " + playerHealth, 20, 90)
+    text("XP to level up: " + (xp_to_level_up - xp_count), 20, 140)
+    text("Bullets remaining: " + bullet_amount, 20, 190)
     level_up();
     pop();
 }
 
+
 function death_screen() {
-    push()
-    background(255, 0, 0)
-    textSize(50)
-    textAlign(CENTER)
-    fill(0, 0, 0)
-    text("You Have Been Corrupted by the Matrix", width / 2, height / 2 - height / 5)
-    text("You Killed: " + enemies_Killed + " Enemies Along the Way.", width / 2, height / 2)
-    text("Press R to Restart", width / 2, height / 2 + 90)
     attack = false
     music = false
-    frameRate(0)
     lvl1_Music.stop()
     lvl2_Music.stop()
     lvl3_Music.stop()
-    pop()
-    if (enemies_Killed > high_score) {
-        high_score = enemies_Killed
-    }
-    enemies_Killed = 0
-    playerHealth = 5
     for (enemy of enemyCount) {
         enemyCount.splice(enemyCount.indexOf(enemy), 20)
+    }
+    for (bullet of bulletCount) {
+        bulletCount.splice(bulletCount.indexOf(bullet), 5)
     }
     for (let i = 0; i < 20; i++) {
         enemyCount.push(new Enemy());
     }
-    playerPos.x = width / 2
-    playerPos.y = height / 2
-    xp_count = 0
-    xp_to_level_up = 12
-    playerSpeed = 2
+    if (typeof end_screen_start_time === 'undefined') {
+        end_screen_start_time = millis()
+    }
+    if (millis() - end_screen_start_time < end_screen_time) {
+        push()
+        background(0, 0, 0)
+        frameRate(7)
+        image.x = random(0, width - 400)
+        image.y = random(0, height - 240)
+        image(gif_loadImg, image.x, image.y)
+        image(gif_loadImg, random(0, width - 400), random(0, height - 240))
+        pop()
+    }
+    else {
+        push()
+        background(255, 0, 0)
+        textSize(50)
+        textAlign(CENTER)
+        fill(0, 0, 0)
+        if (enemies_Killed > high_score) {
+            high_score = enemies_Killed
+        }
+        text("You Have Been Corrupted by the Matrix", width / 2, height / 2 - height / 5)
+        text("You Killed: " + enemies_Killed + " Enemies Along the Way.", width / 2, height / 2)
+        text("Press R to Restart", width / 2, height / 2 + 90)
+        text("Your Highscore: " + high_score, width / 2, height / 2 + 140)
+        frameRate(0)
+        pop()
+        playerPos.x = width / 2
+        playerPos.y = height / 2
+        xp_count = 0
+        xp_to_level_up = 12
+        playerSpeed = 2
+        enemySpeed = 1
+        enemies_Killed = 0
+        playerHealth = 5
+        bullet_amount = 30
+        easy_mode = true
+        medium_mode = false
+        hard_mode = false
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,32 @@
 // GAME VARIABLES:
-let enemies_Killed = 0
-let playerHealth = 5
-let Screen = 1
 let matrixGif;
 let music = false
+let Screen = 1
+let arrowY;
+
+let enemies_Killed = 0
+let playerHealth = 5
+
+let ammo_crate_drop = false
+let ammo_crate_spawn_time = 20000 // 20 Seconds
+let ammo_crate_start_time;
+let ammo_crate_x;
+let ammo_crate_y;
+let ammo_crate_size = 50
+
+let end_screen_time = 800 // matrix glitch effect lenght
+let end_screen_start_time;
+
+let timer;
+let controls = false
+
+let easy_mode = true
+let medium_mode = false
+let hard_mode = false
+
+let attack = false
+let locked = false
+
 let matrixSymbols =
   ["日", "ﾊ", "ﾐ", "ﾋ", "ｰ", "ｳ", "ｼ", "ﾅ", "ﾓ", "ﾆ", "ｻ", "ﾜ", "ﾂ", "ｵ",
     "ﾘ", "ｱ", "ﾎ", "ﾃ", "ﾏ", "ｹ", "ﾒ", "ｴ", "ｶ", "ｷ", "ﾑ", "ﾕ", "ﾗ", "ｾ", "ﾈ",
@@ -13,20 +36,21 @@ let matrixSymbols =
 
 // PLAYER VARIABLES:
 let playerSize = 40;
-let healthBar_pos = playerSize / 1.5
-let xp_count = 0
-let xp_to_level_up = 12
 let playerSpeed = 2
 let playerPos;
+let show_Player_2 = false
+let player2_Display_Time = 100;
+let player2_Display_Start_Time;
+
+let xp_count = 0
+let xp_to_level_up = 12
+let high_score = 0
+
+
 let bulletCount = [];
 let bulletSpeed = 12;
 let bulletSize = 6;
-let show_Player_2 = false
-let player2_Display_Time = 100;
-let level_up_text_time = 10000;
-let player2_Display_Start_Time;
-let level_up_text_start_time;
-let high_score = 0
+let bullet_amount = 30
 
 // ENEMY VARIABLES:
 let enemyCount = [];
@@ -35,32 +59,42 @@ let enemySize = 30;
 let enemyTarget;
 
 // Loads a gif used for the menu screen. 
-// Also loads the music and images.
+// Also loads the music, images and sfx.
 function preload() {
   gif_loadImg = loadImage("assets/matrix.gif");
+  School_Project_BG = loadImage("assets/BG.png")
+  player_1 = loadImage("assets/player_1.png ");
+  player_2 = loadImage("assets/player_2.png ");
+
   menu_Music = loadSound("assets/Menu.mp3")
   lvl1_Music = loadSound("assets/lvl1.mp3")
   lvl2_Music = loadSound("assets/lvl2.mp3")
   lvl3_Music = loadSound("assets/lvl3.mp3")
   enemy_death = loadSound("assets/death.mp3")
-  School_Project_BG = loadImage("assets/BG.png")
-  player_1 = loadImage("assets/player_1.png ");
-  player_2 = loadImage("assets/player_2.png ");
   glock19_shoot = loadSound("assets/glock19-shoot.mp3");
 }
 
 // Level up
 function level_up() {
   if (xp_count >= xp_to_level_up) {
-    console.log(xp_count)
     playerSpeed = playerSpeed * 1.2
-    xp_to_level_up = floor(xp_to_level_up * 1.8)
+    xp_to_level_up = floor(xp_to_level_up * 1.5)
     xp_count = 0
-    playerHealth = 5
-    level_up_text_start_time = millis();
-    if (millis() + level_up_text_start_time < level_up_text_time) {
-      text("GRE", width / 2, height / 2)
-    }
+    bullet_amount += floor(xp_to_level_up / 2)
+    if (playerHealth < 5)
+      playerHealth += 1
+  }
+}
+
+function Ammo_Crate() {
+  if (!ammo_crate_drop && millis() - ammo_crate_start_time > ammo_crate_spawn_time) {
+    rectMode(CENTER, CENTER)
+    fill(255, 255, 0);
+    rect(ammo_crate_x, ammo_crate_y, ammo_crate_size, ammo_crate_size);
+  }
+  if (dist(playerPos.x, playerPos.y, ammo_crate_x, ammo_crate_y) < (playerSize + ammo_crate_size) / 2) {
+    ammo_crate_start_time = undefined;
+    bullet_amount += 20
   }
 }
 
@@ -93,7 +127,7 @@ function healthBar() {
 function cross_hair() {
   push()
   translate(mouseX, mouseY)
-  fill(255, 0, 0)
+  fill(0, 100, 255)
   rectMode(CENTER)
   rect(0, 0, 3, 3)
   rect(0, -13, 3, 6)
@@ -258,7 +292,7 @@ class Enemy {
             const awayVector = p5.Vector.sub(currentEnemy.enemyPos, otherEnemy.enemyPos).normalize();
 
             // Adjust the position of the current enemy away from the other enemy
-            const moveDistance = enemySize - distance;
+            const moveDistance = (enemySize - distance);
             currentEnemy.enemyPos.add(awayVector.mult(moveDistance));
           }
         }
@@ -325,38 +359,109 @@ function setup() {
 }
 
 function keyPressed() {
-  if (keyCode === (27) && Screen == 2 || keyCode === (80) && Screen == 2) {
+  if (Screen == 2 && keyCode === (80)) {
     attack = false
     frameRate(0)
+    locked = true
+    requestPointerLock()
   }
   if (keyCode === (13) && Screen == 2) {
     attack = true
     frameRate(60)
+    exitPointerLock();
+    locked = false;
   }
   if (keyCode == (82) && Screen == 3) {
     Screen = 1
     frameRate(60)
-    attack = true
+  }
+
+  //Difficulty cycling
+  if (keyCode == (65) && Screen == 1 || keyCode == (LEFT_ARROW) && Screen == 1) {
+    // Cycle difficulty to the left:
+    if (easy_mode) {
+      easy_mode = false
+      hard_mode = true
+    }
+    else if (hard_mode) {
+      hard_mode = false
+      medium_mode = true
+    }
+    else if (medium_mode) {
+      medium_mode = false
+      easy_mode = true
+    }
+
+  }
+  if (keyCode == (68) && Screen == 1 || keyCode == (RIGHT_ARROW) && Screen == 1) {
+    //  Cycle difficulty to the right:
+    if (easy_mode) {
+      easy_mode = false
+      medium_mode = true
+    }
+    else if (medium_mode) {
+      medium_mode = false
+      hard_mode = true
+    }
+    else if (hard_mode) {
+      hard_mode = false
+      easy_mode = true
+    }
   }
 }
 
 function mousePressed() {
-  if (mouseButton == LEFT && attack == true && bulletCount.length < 3) {
+  arrowY = height - height / 6
+  if (mouseButton == LEFT && attack == true && bullet_amount > 0) {
     for (let i = 0; i < 1; i++) {
       bulletCount.push(new Bullet(playerPos.x, playerPos.y));
     }
     show_Player_2 = true;
     player2_Display_Start_Time = millis();
     glock19_shoot.play()
+    bullet_amount -= 1
+  }
+  //left button
+  if (mouseX <= width / 2 - 310 && mouseX >= width / 2 - 415 && mouseY < arrowY + 120 && mouseY > arrowY - 70 && easy_mode == true && mouseButton == LEFT && Screen == 1) {
+    easy_mode = false
+    hard_mode = true
+  }
+  else if (mouseX <= width / 2 - 310 && mouseX >= width / 2 - 415 && mouseY < arrowY + 120 && mouseY > arrowY - 70 && hard_mode == true && mouseButton == LEFT && Screen == 1) {
+    medium_mode = true
+    hard_mode = false
+  }
+  else if (mouseX <= width / 2 - 310 && mouseX >= width / 2 - 415 && mouseY < arrowY + 120 && mouseY > arrowY - 70 && medium_mode == true && mouseButton == LEFT && Screen == 1) {
+    easy_mode = true
+    medium_mode = false
+  }
+
+  //right button
+  else if (mouseX >= width / 2 + 310 && mouseX <= width / 2 + 415 && mouseY < arrowY + 120 && mouseY > arrowY - 70 && easy_mode == true && mouseButton == LEFT && Screen == 1) {
+    easy_mode = false
+    medium_mode = true
+  }
+  else if (mouseX >= width / 2 + 310 && mouseX <= width / 2 + 415 && mouseY < arrowY + 120 && mouseY > arrowY - 70 && medium_mode == true && mouseButton == LEFT && Screen == 1) {
+    medium_mode = false
+    hard_mode = true
+  }
+  else if (mouseX >= width / 2 + 310 && mouseX <= width / 2 + 415 && mouseY < arrowY + 120 && mouseY > arrowY - 70 && hard_mode == true && mouseButton == LEFT && Screen == 1) {
+    easy_mode = true
+    hard_mode = false
   }
 }
 
 function draw() {
   // Start Screen
   if (Screen == 1) {
+    cursor(ARROW)
     menuScreen()
     // Play Screen
-  } else if (Screen == 2) {
+  }
+  else if (Screen == 1.5) {
+    controls_screen();
+    noCursor()
+  }
+  else if (Screen == 2) {
     play_screen()
     attack = true
     // Death Screen
